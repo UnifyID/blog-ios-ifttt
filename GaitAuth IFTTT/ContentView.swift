1c74e5d0fa1eeb355628c1d35ef7ea45dfae82b3
Ryan Peden <ryan04@gmail.com>
2020-12-17 08:25:55 -0500

    Initial Commit

---
+++ GaitAuth IFTTT/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GaitAuth IFTTT
+//
+//  Created by Ryan Peden on 2020-12-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
